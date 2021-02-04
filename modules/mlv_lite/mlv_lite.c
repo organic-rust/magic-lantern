1fd2f6ba5ca3f55e7227506b36ff22f53d37854f
dannephoto <dudek53@gmail.com>
2021-02-04 09:45:18 +0100

    mlv_lite.c:(EOSM correcting typo)

--- modules/mlv_lite/mlv_lite.c
+++ modules/mlv_lite/mlv_lite.c
@@ -4355,7 +4355,7 @@ static int raw_rec_should_preview(void)
         if ((raw_active_height > 1300 || raw_active_width > 1800) && RAW_IS_IDLE) preview_mode = 2;
         if (raw_active_height < 1300 && raw_active_width < 1801 && RAW_IS_IDLE) preview_mode = 1;
         //enable framing for 48fps mode no ratio and set_25fps eosm
-        if (shamem_read(0xc0f06014) == 0x68c) preview_mode = 2;'
+        if (shamem_read(0xc0f06014) == 0x68c) preview_mode = 2;
         //When anamorphic_rewired_flv_eosm !ratios && set_25fps
         if (shamem_read(0xc0f06804) == 0x90701e4) preview_mode = 2;
     }
